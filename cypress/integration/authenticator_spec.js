80c0e49c2c6fa1675dfde9818cdd6648bb85bff1
YukihiroEhara <yukihiro.ehara@brandcloud.co.jp>
2022-07-08 14:13:35 +0900

    test

--- cypress/integration/authenticator_spec.js
+++ cypress/integration/authenticator_spec.js
@@ -1,13 +1,12 @@
 describe("Authenticator:", function () {
   beforeEach(function () {
-    cy.visit("/");
+    cy.visit("https://main.ddjj0z1x4mm5d.amplifyapp.com/#/");
   });
   describe("Sign In:", () => {
     it("allows a user to signin", () => {
       cy.get(selectors.usernameInput).type("e2e_test");
       cy.get(selectors.signInPasswordInput).type("ot1928d1");
       cy.get(selectors.signInSignInButton).contains("Sign In").click();
-      cy.wait(3000);
       cy.get(selectors.root).contains("Global");
     });
   });
